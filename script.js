54e4b268eb9d7dafed5614a4a81ba2d1576d940e
Frances <hisimon299@gmail.com>
2022-11-29 00:11:20 -0500

    js hw

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 // put your globals here - hint: select elements from the HTML
+const textArea = document.querySelector("#text-area");
+const countBtn = document.querySelector("#countBtn");
+const wordCountInput = document.querySelector("#wordCountInput");
+const findWordInput = document.querySelector("#findWordInput");
+const findBtn = document.querySelector("#findBtn");
+const wordFoundInput = document.querySelector("#wordFoundInput");
+
+countBtn.addEventListener('click', countWords);
+findBtn.addEventListener('click', findWords);
 
 
 // dont' forget to add event listeners to teh buttons
 
 function countWords() {
+  const textContent = textArea.value;
+  const arr = textContent.split(" ");
+  printData(arr.length, wordCountInput);
   // your code here
   // call printData
 }
 
 function findWords() {
+  const foundWords = [];
+  const textContent = textArea.value;
+  const arr = textContent.split(" ");
+  arr. forEach( (item) => {
+    if (item === findWordInput.value) {
+      foundWords.push(item);
+    }
+  });
+  printData(foundWords.length, wordFoundInput);
   // put your local variable for the empty array here
   // remainder of your code follows
   // call printData
 }
 
 // change param1 and param2 to identifiers that make sense
-function printData(param1, param2) {
+function printData(data, destination) {
+  destination.value = data;
   // your code here... one line!
+  /* if I want to add words after the number
+  
+    if (destination == wordCountInput){
+    destination.value = data + ' ' + 'words';
+  } else {
+    destination.value = data + ' ' + 'instances';
+  }
+  */
 }
